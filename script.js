bed3ebcea92837b4d85984b500518fc6e1f5a3d8
mahdi nobahar <nobahar.mahdi@yahoo.com>
2024-04-22 23:02:25 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".partner-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 5,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
